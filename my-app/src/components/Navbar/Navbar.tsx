98600f6c92ac08687ba762fbb2b014b3cce4e5e5
DraksBG <52380770+DraksBG@users.noreply.github.com>
2021-11-03 16:42:54 +0200

    Added the navbar

--- my-app/src/components/Navbar/Navbar.tsx
+++ my-app/src/components/Navbar/Navbar.tsx
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
     return (
-        <div>
-            Nav
-        </div>
+        <nav className={styles.navbar}>
+            <ul>
+                <li className={styles.title}>
+                    myMoney
+                </li>
+                <li><Link to="/login">Login</Link></li>
+                <li><Link to="/signup">Signup</Link></li>
+            </ul>
+        </nav>
     )
 }
