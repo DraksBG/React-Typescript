41a5af574352129b411bcad8cc9eca6976dd082c
DraksBG <dimitarkrustanov@gmail.com>
2021-11-03 16:18:55 +0200

    Inital work started

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ my-app/src/pages/signup/Signup.tsx
@@ -0,0 +1,9 @@
+import styles from 'Signup.module.css';
+
+export default function Signup() {
+    return (
+        <div>
+            Signup
+        </div>
+    )
+}
